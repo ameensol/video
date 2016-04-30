85ab25c1babb926e716e3dd58b87e17c878f3eea
ameen <ameensol@gmail.com>
2016-04-30 16:57:36 -0400

    buttons are purple

---
+++ contracts/App.sol
@@ -0,0 +1,21 @@
+// This contract manages the application. That includes tracking user accounts, balances, reputation, and active video contracts.
+
+contract App {
+
+  struct Tutor {
+    string name;
+    string[] skills;
+    uint reputation;
+  }
+
+  struct Student {
+    string name;
+    uint balance;
+    uint reputation;
+  }
+
+  mapping (address => Tutor) public tutors;
+  mapping (address => Student) public students;
+
+
+}
